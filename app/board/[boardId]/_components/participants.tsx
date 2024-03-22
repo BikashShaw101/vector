96274fd99b06eade644d2354f2466ac80c2c9804
bikashshaw <bikashvicky@gmail.com>
2024-03-22 17:37:16 +0530

    skeleton and canvas design with liveblocks

--- app/board/[boardId]/_components/participants.tsx
+++ app/board/[boardId]/_components/participants.tsx
@@ -1,3 +1,13 @@
 export const Participants = () => {
-  return <div></div>;
+  return (
+    <div className="absolute h-12 top-2 right-2 flex bg-white rounded-md p-3 items-center shadow-md">
+      list of user
+    </div>
+  );
+};
+
+Participants.Skeleton = function ParticipantsSkeleton() {
+  return (
+    <div className="absolute h-12 top-2 right-2 flex bg-white rounded-md p-3 items-center shadow-md w-[100px]" />
+  );
 };
