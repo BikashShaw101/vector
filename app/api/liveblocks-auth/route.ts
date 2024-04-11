bcea1a726b52867e5fc53ada48f0d0d429b75cd0
bikashshaw <bikashvicky@gmail.com>
2024-04-11 20:29:07 +0530

    secret key transparency

--- app/api/liveblocks-auth/route.ts
+++ app/api/liveblocks-auth/route.ts
@@ -7,8 +7,7 @@ import { api } from "@/convex/_generated/api";
 const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 const liveblocks = new Liveblocks({
-  secret:
-    "sk_dev_-DRR6M3LG1cTaHt546mgH_SqbIykzYTvxhW5gBnT03tRmy5nk_QnK9CFmObTQvwy",
+  secret: process.env.LIVEBLOCKS_SECRET_KEY!,
 });
 
 export async function POST(request: Request) {
